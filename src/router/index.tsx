715709dfabe209a9bee9c8629a761066caba9bca
final <3486159271@qq.com>
2026-04-12 22:07:10 +0800

    fix: update route path for TimelinePage from /timeline to /detail

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -6,7 +6,7 @@ export default function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
-      <Route path="/timeline" element={<TimelinePage />} />
+      <Route path="/detail" element={<TimelinePage />} />
     </Routes>
   )
 }
